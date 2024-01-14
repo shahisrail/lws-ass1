d846f8bb35224e83522f20dc294499ae5870a28b
FalconTeam2024 <teamfalcon.or@gmail.com>
2024-01-15 00:27:52 +0600

    make a nabbar and make hero sectoin

---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,22 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div className="bg-[radial-gradient(50%_50%_at_50%_50%,#17956D_0%,#0F684C_100%)] pt-32 pb-20 -mt-[92px] md:-mt-[118px] flex justify-center gap-4 p-[180px] items-center">
+      <div className="flex-1">
+        <h2 className="text-4xl lg:text-[56px] font-bold leading-[1.1] mb-8 text-white">
+          The Future of Learning starts with students at the center
+        </h2>
+        <button className="px-5 py-2.5 bg-[#038C61] rounded-[44px] text-white">
+          Learn More
+        </button>
+      </div>
+      <div className="">
+        <img src="/src/assets/hero-graphics.svg" alt="Banner" />
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
---
+++ src/Components/Nav/Nav.jsx
@@ -0,0 +1,20 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+
+const Nav = () => {
+  return (
+    <div className="flex justify-between p-5">
+      <div>
+        <img  src="/src/assets/lws-logo-en.svg" alt="Lws" />
+      </div>
+      <div>
+
+        <button className="px-5 py-2 bg-[#172227] rounded-[44px] text-white font-bold ">
+        Get Admission
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Nav;
